2cc45b6cf6a6614fa48dde215ddd7062fca14d03
duongminhlonge <duongminhlonge@gmail.com>
2016-03-21 11:31:48 +0700

    fff

---
+++ application/view/v_add.php
@@ -0,0 +1,7 @@
+<div div="row">
+	<div class="col-xs-12 col-md-3">
+		<button type="button" class="btn btn-danger" id="btnAdd-student" data-toggle="modal" data-target="#modal-add-student">Thêm sinh viên</button>
+	</div>
+</div>
+
+<br />
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet"  href="asset/bootstrap/css/bootstrap.css">
+	
+	<!--JQuery-->
+	<script src="asset/js/jquery.min.js"></script>
+
+	<!--Bootstrap JS-->
+	<script src="asset/bootstrap/js/bootstrap.min.js"></script>
+
+	<!--JQquery validation-->
+	<script type="text/javascript" src="asset/js/jquery.validate.min.js"></script>
+
+	</head>
+<body>
+	<div class="container">
+		<!--Title-->
+		<div div="row">
+			<div class="col-xs-12 col-md-12">
+				<h1 style="text-align: center">Quản lý sinh viên</h1>
+			</div>
+		</div><!--Title-->
+
+		<!--Add student-->
+		<?php  
+			include("application/view/v_add.php");
+		?>
+		<!--Add student-->
+
+
+		<!--Table Data-->
+		<?php  
+			include("application/view/v_listall.php");
+		?>
+		<!--Table Data-->
+	</div><!--Container-->
+</body>
+</html>
