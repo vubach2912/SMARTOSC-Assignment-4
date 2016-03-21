83a2a1d3cd3f2b68b2d2e37838af71d9c232e1e5
duongminhlonge <duongminhlonge@gmail.com>
2016-03-21 11:09:37 +0700

    view list_all

---
+++ application/view/v_listall.php
@@ -0,0 +1,48 @@
+<!--Show data-->
+<div div="row">
+	<table class="table table-striped table-hover">
+		<thead>
+			<tr>
+				<th>STT</th>
+				<th>ProductID</th>
+				<th>ProductName</th>
+				<th>Price</th>
+				<th>Edit</th>
+				<th>Delete</th>
+				<th>Add to cart</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php 
+				//Neu co du lieu sinh vien
+				if(isset($student) and count($student)){
+					foreach ($student as $key => $value) {
+			?>
+						<tr>
+							<th><?php echo $value['id']; ?></th>
+							<th><?php echo $value['name']; ?></th>
+							<th><?php echo $value['email']; ?></th>
+							<th><?php echo $value['address']; ?></th>
+							<th><?php echo $value['phone']; ?></th>
+							<th><?php echo $value['gender']; ?></th>
+							<th><?php echo $value['country']; ?></th>
+							<td><a href="" class="btnEdit_product_show" data-toggle="modal" data-target="#modal-edit-product" data-product-id="<?php echo $value['pro_id']; ?>" data-product-name="<?php echo $value['pro_name']; ?>" data-price="<?php echo $value['pro_price']; ?>"><span class="glyphicon glyphicon-download-alt" aria-hidden="true" style="color: red"></span> Sửa</a></td>
+							<td><a href="<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]".'&id_product='.$value['pro_id']; ?>"><span class="glyphicon glyphicon-trash" aria-hidden="true" style="color: red"></span> Xóa</a></td>
+						</tr>
+			<?php
+					}
+				}
+				//Neu khong co du lieu
+				else{
+			?>
+					<tr>
+						<th><?php echo "Không có dữ liệu!"; ?></th>	
+					</tr>
+			<?php
+				}
+			?>
+		</tbody>
+
+	</table>
+</div>
--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-hiha
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet"  href="asset/bootstrap/css/bootstrap.css">
+	
+	<!--JQuery-->
+	<script src="asset/js/jquery.min.js"></script>
+
+	<!--Bootstrap JS-->
+	<script src="asset/bootstrap/js/bootstrap.min.js"></script>
+
+	<!--JQquery validation-->
+	<script type="text/javascript" src="asset/js/jquery.validate.min.js"></script>
+
+	</head>
+<body>
+	<div class="container">
+		<!--Title-->
+		<div div="row">
+			<div class="col-xs-12 col-md-12">
+				<h1 style="text-align: center">Quản lý sinh viên</h1>
+			</div>
+		</div><!--Title-->
+
+		
+
+		<!--Table Data-->
+		<?php  
+			include("application/view/v_listall.php");
+		?>
+		<!--Table Data-->
+	</div><!--Container-->
+</body>
+</html>
