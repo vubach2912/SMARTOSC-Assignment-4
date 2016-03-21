4d3e0a2f51138817fcdbd468e54a42f71cd4cfff
Quang Huy <anybao4@gmail.com>
2016-03-21 11:46:34 +0700

    update Pagination file

--- application/lib/pagination.php
+++ application/lib/pagination.php
@@ -1,20 +1,20 @@
 <?php
 	class Pagination{
-		private $config = array();
-		private $base_url;
-		private $total_rows;
-		private $per_page;
-		private $start_from;
+		private var $config = array();
+		private var $base_url;
+		private var $total_rows;
+		private var $per_page;
+		private var $start_from;
 
 		function __construct(){
 
 		}
 
 		public function config($config){
-			$this->$base_url = $config['base_url'];
-			$this->$total_rows = $config['total_rows'];
-			$this->$per_page = $config['per_page'];
-			$this->$start_from = ($config['total_rows'] - 1)*$config['per_page'];
+			$this->base_url = $config['base_url'];
+			$this->total_rows = $config['total_rows'];
+			$this->per_page = $config['per_page'];
+			$this->start_from = ($config['total_rows'] - 1)*$config['per_page'];
 
 			$query = mysql_query("SELECT * FROM student ORDER BY id DESC LIMIT $start_from, $per_page");
 		}
@@ -25,11 +25,11 @@
 				$total_records = mysql_num_rows($rs_result);  //count number of records
 				$total_pages = ceil($total_records / $num_rec_per_page); 
 
-				echo '<a href="'.$this->$base_url.'?page=1>"First page </a>';
+				echo '<a href="'.$this->base_url.'?page=1>"First page </a>';
 				for ($i=1; $i<=$total_pages; $i++) { 
 				            echo "<a href='product.php?page=".$i."'>".$i."</a> "; 
 				}; 
-				echo '<a href="'.$this->$base_url.'?page='.$this->total_pages.'>"First page </a>';
+				echo '<a href="'.$this->base_url.'?page='.$this->total_pages.'>"First page </a>';
 		}
 	}
 
