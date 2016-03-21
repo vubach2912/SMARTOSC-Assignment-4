6508a6d3218b9ff982236d24f0d390cb3f96d6c1
vubach2912 <vubach2912@gmail.com>
2016-03-21 11:35:17 +0700

    Update model

--- application/model/m_database.php
+++ application/model/m_database.php
@@ -1,1 +1,106 @@
-m_database.php
+<?php
+class m_database
+{
+    var $servername = "localhost";
+    var $username = "root";
+    var $password = "1234";
+    var $database = "smartosc_assignment3_bachvu";
+    var $conn = null;
+
+    /**
+     * dbconnect constructor.
+     */
+    public function __construct(){
+
+    }
+
+    /*
+     * OPEN DATABASE
+     */
+    public function openConnect(){
+        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+        if (!$this->conn) {
+            return false;
+        } return true;
+    }
+
+    /*
+     * CLOSE DATABASE AFTER CONNECT
+     */
+    public function closeConnect(){
+        mysqli_close($this->conn);
+        $this->conn = null;
+    }
+    /*
+     *  SELECT ALL VALUE IN TABLE TO ARRAY
+     */
+    public function selectAllQuery($sql){
+        if($sql != '')
+        {
+            $this->openConnect();
+            $list = mysqli_query($this->conn,$sql);
+            $result = array();
+            if( mysqli_num_rows($list) > 0)
+            {
+                while($row = mysqli_fetch_array($list))
+                {
+                    $result[] = $row;
+                }
+                $this->closeConnect();
+                return $result;
+            }
+            else
+            {
+                return false;
+            }
+        } else return false;
+    }
+
+    /*
+     *  SELECT 1 VALUES
+     */
+    public function selectQuery($sql){
+        if($sql != '')
+        {
+            $this->openConnect();
+            $list = mysqli_query($this->conn,$sql);
+            $result = array();
+            if(!$list){} else {
+                if(mysqli_num_rows($list) > 0)
+                {
+                    while($row = mysqli_fetch_array($list))
+                    {
+                        $result = $row;
+                    }
+                    $this->closeConnect();
+                    return $result[0];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        } else return false;
+    }
+
+    /*
+	 *	Query to update, delete processor
+     */
+
+    public function executeQuery($sql){
+        if($sql != '')
+        {
+            $this->openConnect();
+            $execute = mysqli_query($this->conn,$sql);
+            if($execute)
+            {
+                $this->closeConnect();
+                return true;
+            }
+            else
+                return false;
+
+        } else return false;
+    }
+}
+?>
--- application/model/m_student.php
+++ application/model/m_student.php
@@ -1,1 +1,103 @@
-m_student.php
+<?php
+/*
+ * 
+ */
+require_once('../../application/model/m_database.php');
+class m_student{
+	var $db = new m_database;
+	var $id;
+	var $username;
+	var $email;
+	var $phone;
+	var $gender;
+	var $address;
+	var $country;
+	
+	public function __construct($id,$username,$email,$address,$phone,$gender,$country){
+		$this->id = $id;
+		$this->username = $username;
+		$this->email = $email;
+		$this->address = $address;
+		$this->phone = $phone;
+		$this->gender = $gender;
+		$this->country = $country;
+    }
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getUsername(){
+		return $this->username;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function getAddress(){
+		return $this->address;
+	}
+
+	public function getPhone(){
+		return $this->phone;
+	}
+
+	public function getGender(){
+		return $this->gender;
+	}
+
+	public function getCountry(){
+		return $this->country;
+	}
+
+	public function setUsername($username){
+		$this->username = $username;
+        if($this->id != 0){
+		    $sql = "UPDATE `tbl_sinhvien` SET `name` = '$username' WHERE id = '$this->$id'";
+		    $this->db->executeQuery($sql);
+        }
+	}
+
+	public function setEmail($email){
+        $this->email = $email;
+        if($this->id != 0){
+            $sql = "UPDATE `tbl_sinhvien` SET `email` = '$email' WHERE id = '$this->$id'";
+            $this->db->executeQuery($sql);
+        }
+	}
+
+	public function setAddress($address){
+        $this->address = $address;
+        if($this->id != 0){
+            $sql = "UPDATE `tbl_sinhvien` SET `address` = '$address' WHERE id = '$this->$id'";
+            $this->db->executeQuery($sql);
+        }
+	}
+
+	public function setPhone($phone){
+        $this->phone = $phone;
+        if($this->id != 0){
+            $sql = "UPDATE `tbl_sinhvien` SET `phone` = '$phone' WHERE id = '$this->$id'";
+            $this->db->executeQuery($sql);
+        }
+	}
+
+	public function setGender($gender){
+        $this->gender = $gender;
+        if($this->id != 0){
+            $sql = "UPDATE `tbl_sinhvien` SET `gender` = '$gender' WHERE id = '$this->$id'";
+            $this->db->executeQuery($sql);
+        }
+	}
+
+	public function setCountry($country){
+        $this->country = $country;
+        if($this->id != 0){
+            $sql = "UPDATE `tbl_sinhvien` SET `country` = '$country' WHERE id = '$this->$id'";
+            $this->db->executeQuery($sql);
+        };
+	}
+	
+}
+?>
